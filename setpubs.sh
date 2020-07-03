e0846f7ace5115db991453c92450e368b2eaa6be
dvklopfenstein <dvklopfenstein@users.noreply.github.com>
2020-07-03 16:54:20 -0400

    Added PYTHONPATH

--- setpubs.sh
+++ setpubs.sh
@@ -1,4 +1,5 @@
 # Use the bibliography in this repo
+export PYTHONPATH=.:./src:./src/py:${GIT}/dvk:${GIT}/pubscite/src:${GIT}/pmidcite/src:${GIT}/bibliography/src:${GIT}/pubs:${GIT}/enrichmentanalysis/src:${GIT}/ReactomePy/src:${GIT}/flashcards/src:${GIT}/biodnld/src:${GIT}/biocode/src:${GIT}/goatools_alpha/src:${GIT}/goatools/:${GIT}/genanki/:${GIT}/prtgitlog/src:${GIT}/PrincetonAlgorithms/py/
 
 #echo '!!!!!! WARNING: YOU MUST RUN: source setpubs.sh !!!!!!!'
 echo 'PUBSCONF BEFORE:' $PUBSCONF
